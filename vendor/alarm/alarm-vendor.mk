6a9ae9d28bfe6c927e8be4fe856f676d58f4a692
Kujou Yuko <i@779.moe>
2025-07-25 22:18:20 +0900

    vendor: alarm: Update to LA.VENDOR.15.4.0.r1-15000-pakala.0-1
    Change-Id: Ic510fa8a315401e4e8ee32904e8d6bebe2ea052f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- vendor/alarm/alarm-vendor.mk
+++ vendor/alarm/alarm-vendor.mk
@@ -6,10 +6,11 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/common/vendor/alarm
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/common/vendor/alarm/proprietary/vendor/etc/init/vendor.qti.hardware.alarm@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.alarm@1.0-service.rc
+    vendor/qcom/common/vendor/alarm/proprietary/vendor/etc/init/vendor.qti.hardware.alarm-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.alarm-service.rc
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.alarm@1.0-impl \
-    vendor.qti.hardware.alarm@1.0 \
-    vendor.qti.hardware.alarm@1.0-service \
+    vendor.qti.hardware.alarm-impl \
+    vendor.qti.hardware.alarm-V1-ndk \
+    vendor.qti.hardware.alarm.xml \
+    vendor.qti.hardware.alarm-service \
     power_off_alarm
